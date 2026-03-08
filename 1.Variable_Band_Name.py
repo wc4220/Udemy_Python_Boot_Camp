f101158a739a3880b615dd33955e9e58b8f0319c
Rich Choi <woohyuck4220@gmail.com>
2026-03-08 16:32:35 +1100

    First project complete

---
+++ 1.Variable_Band_Name.py
@@ -0,0 +1,11 @@
+print("Welcome to the band name generator!")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's the name of a pet you have?")
+pet = input()
+print("Your band name could be" + " " + city + " " + pet +"!")
+
+
+# \n creates a new line
+print("Welcome to the band name generator!\nWhat's the name of the city you grew up in?")
+city = input() 
